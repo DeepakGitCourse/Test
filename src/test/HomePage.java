5f4a10ee50d93538b6c970d3fe76706091bcba93
DeepakGitCourse <deepak.gitcourse@gmail.com>
2021-12-05 12:41:58 +0530

    Second commit

--- src/test/HomePage.java
+++ src/test/HomePage.java
@@ -1,10 +1,9 @@
 package test;
 
-public class HelloWorld {
+public class HomePage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
 
 	}
 
